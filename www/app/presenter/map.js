c33a1a64f2a04b0aede3bbed420e7427d4f83a25
Matt Wallis <matt.wallis@tickpea.com>
2016-04-18 07:47:49 +0100

    Separate from map presenter code that is sepecific to Waypoint Sequences.

--- www/app/presenter/map.js
+++ www/app/presenter/map.js
@@ -1,4 +1,4 @@
-define(["leaflet", "app/eventbus"], function(leaflet, eventbus) {
+define(["app/eventbus", "presenter/map/wptseq"], function(eventbus, wptseqPresenter) {
 	"use strict";
 
 	var views = [];
@@ -6,56 +6,15 @@ define(["leaflet", "app/eventbus"], function(leaflet, eventbus) {
 		views.push(v);
 		return { };	// return settings
 	}
-	// See
-	// http://leafletjs.com/reference.html#events
-	// http://leafletjs.com/reference.html#event-objects
-	// http://leafletjs.com/reference.html#path - the Event types are defined here.
-	var eventHandlers = {
-		// We need to keep the reference to each waypintSequence (in the model)
-		// within (as a closure) each event handler we create:
-		click: function(wptseq) {
-			return function(e) {
-				console.log("WaypointSequence click: " + e.latlng);
-				// Tell the model to do something:
-				wptseq.setSelected(true);
-			};
-		},
-		mouseover: function(wptseq) {
-			return function(e) {
-				//console.log("WaypointSequence mouseover: " + e.latlng);
-				// Tell the model to do something:
-				wptseq.setHovered(true);
-			};
-		},
-		mouseout: function(wptseq) {
-			return function(e) {
-				//console.log("WaypointSequence mouseout: " + e.latlng);
-				// Tell the model to do something:
-				wptseq.setHovered(false);
-			};
-		}
-	};
-	function createEventHandlers(wptseq) {
-		var res = {};
-		var i, keys = Object.keys(eventHandlers);
-		for (i = 0; i < keys.length; ++i) {
-			res[keys[i]] = eventHandlers[keys[i]](wptseq);
-		}
-		return res;
-	}
 	function onNewWaypointSequence(data/*, envelope*/) {
 		//console.log("onNewWaypointSequence");
-		var i, j;
+		var i;
 		var seq = data.waypointSequence;
-		var latLngs = [];
-		//console.log(seq.length);
-		for (j = 0; j < seq.length; ++j) {
-			latLngs.push(leaflet.latLng(seq.item(j).lat, seq.item(j).lon));
-		}
+		var latLngs = wptseqPresenter.toLeafletLatLngs(seq);
 		for (i = 0; i < views.length; ++i) {
 			// Here, we register eventHandlers with each view. If the model changes state
 			// as a result of handling these events, we will pick up those state changes in onWaypointSequenceStateChange.
-			seq.setUserData({mapView: views[i].addWaypointSequence(latLngs, createEventHandlers(seq))});
+			seq.setUserData({mapView: views[i].addWaypointSequence(latLngs, wptseqPresenter.createEventHandlers(seq))});
 		}
 	}
 	function onWaypointSequenceStateChange(data/*, envelope*/) {
---
+++ www/app/presenter/map/wptseq.js
@@ -0,0 +1,54 @@
+define(["leaflet", "app/eventbus"], function(leaflet, eventbus) {
+	"use strict";
+
+	// See
+	// http://leafletjs.com/reference.html#events
+	// http://leafletjs.com/reference.html#event-objects
+	// http://leafletjs.com/reference.html#path - the Event types are defined here.
+	var eventHandlers = {
+		// We need to keep the reference to each waypintSequence (in the model)
+		// within (as a closure) each event handler we create:
+		click: function(wptseq) {
+			return function(e) {
+				console.log("WaypointSequence click: " + e.latlng);
+				// Tell the model to do something:
+				wptseq.setSelected(true);
+			};
+		},
+		mouseover: function(wptseq) {
+			return function(e) {
+				//console.log("WaypointSequence mouseover: " + e.latlng);
+				// Tell the model to do something:
+				wptseq.setHovered(true);
+			};
+		},
+		mouseout: function(wptseq) {
+			return function(e) {
+				//console.log("WaypointSequence mouseout: " + e.latlng);
+				// Tell the model to do something:
+				wptseq.setHovered(false);
+			};
+		}
+	};
+	function createEventHandlers(wptseq) {
+		var res = {};
+		var i, keys = Object.keys(eventHandlers);
+		for (i = 0; i < keys.length; ++i) {
+			res[keys[i]] = eventHandlers[keys[i]](wptseq);
+		}
+		return res;
+	}
+	function toLeafletLatLngs(seq) {
+		var j, latLngs = [];
+		for (j = 0; j < seq.length; ++j) {
+			latLngs.push(leaflet.latLng(seq.item(j).lat, seq.item(j).lon));
+		}
+		return latLngs;
+	}
+	var pub = {
+		createEventHandlers: createEventHandlers,
+		toLeafletLatLngs: toLeafletLatLngs
+	};
+	return pub;
+});
+
