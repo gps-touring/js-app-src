a46aec26a266e3db001bb0cb0422b1e8ca365cea
Matt Wallis <matt.wallis@tickpea.com>
2016-04-20 09:06:44 +0100

    Google map now launched showing terrain.

--- www/app/presenter/map.js
+++ www/app/presenter/map.js
@@ -19,9 +19,25 @@ define(["app/eventbus", "model/points", "presenter/map/wptseq"], function(eventb
 			{
 				text: "Launch Google maps",
 				callback: function(e) {
-					// TODO Consider matching Google maps' zoom level with the current view.
-					var zoom = 14;
-					var url = "https://www.google.co.uk/maps/@" + e.latlng.lat.toFixed(7) + "," + e.latlng.lng.toFixed(7) + "," + zoom + "z";
+					// Documentation of Google maps URL parameters from here:
+					// https://moz.com/ugc/everything-you-never-wanted-to-know-about-google-maps-parameters
+					// Example:
+					// https://www.google.co.uk/maps?q=loc:49.8764953,1.1566544&z=14&t=p
+
+					var params = [
+						// TODO - put a marker on Google maps at e.latlng.
+						//"q=loc:" puts a marker on the map, BUT ignores the z and t parameters. Hmmm.
+						//"q=loc:" + e.latlng.lat.toFixed(7) + "," + e.latlng.lng.toFixed(7),
+						"ll=" + e.latlng.lat.toFixed(7) + "," + e.latlng.lng.toFixed(7),
+						"z=14", // zoom. TODO: Consider matching with current view?
+						"t=p" 	// for terrain map
+					];
+					var url = "https://www.google.co.uk/maps?" + params.join("&");
+
+					// This version of the URL also works (and is more modern?). 
+					// But I have not worked out how to specify a terrain map, nor markers.
+					//var zoom = 14;
+					//var url = "https://www.google.co.uk/maps/@" + e.latlng.lat.toFixed(7) + "," + e.latlng.lng.toFixed(7) + "," + zoom + "z";
 					console.log(url);
 					window.open(url, "_blank");
 				}
